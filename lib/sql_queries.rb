367d37aa98003c5a6b2c5f4d93d0431fcf4c842c
Jeremy Parker <jrobinparker@gmail.com>
2018-06-16 23:02:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,19 +7,19 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+  return "SELECT Projects.title, SUM(Pledges.amount) FROM Projects JOIN Pledges ON Pledges.project_id = Projects.id GROUP BY Projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  return "SELECT Users.name, Users.age, SUM(Pledges.amount) FROM Users JOIN Pledges ON Pledges.user_id = Users.id GROUP BY Users.name ORDER BY Users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  return ""
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+ return "SELECT Users.name, SUM(Pledges.amount) FROM Users JOIN Pledges ON Pledges.user_id = Users.id GROUP BY Users.name ORDER BY Pledges.amount, Users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
